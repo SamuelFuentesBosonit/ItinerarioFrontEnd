472b316ff904b9a0042a5500d7b6ad2240bd9b99
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-06-10 13:15:33 +0200

    08 ts

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,17 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-
-class Heroe {
-    alterEgo: String;
-    edad: number;
-    nombreReal: number;
-
-    /*imprimirNombre() {
-        return this.alterEgo + ' ' + this.nombreReal;
-    }*/
-}
-
-const ironman = new Heroe();
-
-console.log(ironman)
