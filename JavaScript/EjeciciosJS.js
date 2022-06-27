a36c547ab6a41a228caace2a3af5548bf55a9293
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-06-27 13:24:29 +0200

    JS-09

--- JavaScript/EjeciciosJS.js
+++ JavaScript/EjeciciosJS.js
@@ -128,3 +128,15 @@ const test = ( val ) => {
   }
 }
 console.log("resultado: " + test(10000000))
+
+//Ejercicio 9
+const myObjLowercase = ( obj ) => {
+  let lowerObj = new Object();
+  Object.entries(obj).map(([key, value]) => {
+    lowerObj[key.toLowerCase()] = value
+  })
+  return lowerObj
+}
+
+let myObject = { NamE: 'Charles', ADDress: 'Home Street' };
+console.log(myObjLowercase(myObject));
