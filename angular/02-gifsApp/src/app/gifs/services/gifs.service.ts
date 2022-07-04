aa5f1886f3a23725a6e758a8e8f40b2db9091598
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-07-04 17:58:56 +0200

    angular-07 Final 02-gifApp

--- angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
@@ -7,10 +7,11 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  private apiKey    : string = 'UIth0bpUaXBjLUrv0bFxFOuWiUxlEah3'
-  private _historial: string[] = [];
+  private apiKey    :   string = 'UIth0bpUaXBjLUrv0bFxFOuWiUxlEah3';
+  private servicioUrl:  string= 'https://api.giphy.com/v1/gifs';
+  private _historial:   string[] = [];
   
-  public resultados: Gif[] = [];
+  public resultados:    Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -18,6 +19,7 @@ export class GifsService {
 
   constructor( private http: HttpClient ) { 
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
     //if(localStorage.getItem('historial') ){
     //  this._historial = JSON.parse( localStorage.getItem('historial')! );
     //}
@@ -32,11 +34,16 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
       localStorage.setItem('historial', JSON.stringify( this._historial ) );
     }
-    
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=UIth0bpUaXBjLUrv0bFxFOuWiUxlEah3&q=${query}&limit=10`)
+
+    const params = new HttpParams()
+      .set( 'api_key', this.apiKey )
+      .set( 'limit', '10' )
+      .set( 'q', query );
+        
+    this.http.get<SearchGifsResponse>( `${ this.servicioUrl}/search`, { params: params } )
     .subscribe( ( resp ) => {
-      console.log(resp.data);
       this.resultados = resp.data;
+      localStorage.setItem( 'resultados', JSON.stringify( this.resultados ) );
     })
   }
 }
