0564bd488bdc1ea143bbc5340068ea29362529d5
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-07-18 13:58:54 +0200

    angular-10 156 04-pipesApp

--- angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -57,5 +58,16 @@ export class NoComunesComponent {
       vuela: false
     }
   ]
+
+  //Async Pipe
+
+  miObservable = interval(2000);
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+      resolve( 'Temenemos data de promesa' );
+    }, 3500
+    );
+  })
 
 }
