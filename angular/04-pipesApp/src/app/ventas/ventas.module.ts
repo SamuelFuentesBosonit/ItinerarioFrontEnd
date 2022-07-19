ffc2c024c7d82e2b7cf050e51da1cff55c19b8b4
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-07-19 16:14:37 +0200

    angular-11 166 04-pipesApp

--- angular/04-pipesApp/src/app/ventas/ventas.module.ts
+++ angular/04-pipesApp/src/app/ventas/ventas.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//components
 import { BasicosComponent } from './pages/basicos/basicos.component';
-import { MayusculasPipe } from './pipes/mayusculas.pipe';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+
+//pipes
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
 
+//NG
 import { PrimeNGModule } from '../prime-ng/prime-ng.module';
+
 
 
 
@@ -16,12 +22,16 @@ import { PrimeNGModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
+    //components
     BasicosComponent,
-    MayusculasPipe,
     NoComunesComponent,
     NumerosComponent,
     OrdenarComponent,
-    VuelaPipe
+    //pipes
+    MayusculasPipe,
+    OrdenarPipe,
+    VuelaPipe,
+    
   ],
   exports: [
     BasicosComponent,
