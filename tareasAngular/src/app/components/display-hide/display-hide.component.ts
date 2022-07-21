96ba3fc54a92bbf0b989c35a4d5fabf32b6b52a2
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-07-21 19:14:20 +0200

    tareasAngular - tarea01 Display&Hide

--- tareasAngular/src/app/components/display-hide/display-hide.component.ts
+++ tareasAngular/src/app/components/display-hide/display-hide.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayHideComponent implements OnInit {
 
+  imgOne    : string = 'assets/images/one.png';
+  imgTwo    : string = 'assets/images/two.gif';
+  isVisible : string = 'visible';
+  eEgg      : string = 'hidden';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+    //display and hide img
+    displayHide(){
+      (this.isVisible === 'visible')
+      ? this.isVisible = 'hidden'
+      : (this.isVisible = 'visible', this.eEgg = 'hidden');
+    }
+    //display the EasterEgg
+    easterEgg(){
+      this.isVisible = 'hidden';
+      this.eEgg = 'visible';
+    }
 
 }
