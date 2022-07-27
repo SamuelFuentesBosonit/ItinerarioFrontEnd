6679e9cab54bfefca004afa5dd7fc270cf2d7e13
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-07-27 18:40:51 +0200

    tareasAngular - tarea03 form iconos

--- tareasAngular/src/app/components/crud/crud-form/crud-form.component.ts
+++ tareasAngular/src/app/components/crud/crud-form/crud-form.component.ts
@@ -8,10 +8,6 @@ import { User } from '../../../interfaces/user.interface';
 
 import { CountriesService } from '../../../services/countries.service';
 
-
-
-
-
 @Component({
   selector: 'app-crud-form',
   templateUrl: './crud-form.component.html',
@@ -20,7 +16,7 @@ import { CountriesService } from '../../../services/countries.service';
 export class CrudFormComponent implements OnInit {
 
   namePattern = "([a-zA-Z]+)( ([a-zA-Z]+))? ([a-zA-Z]+)( ([a-zA-Z]+))?";
-  passPattern = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$";
+  //passPattern = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$";, Validators.pattern ( this.passPattern )
   mailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
   isValidFormSubmitted? : any;
@@ -32,9 +28,7 @@ export class CrudFormComponent implements OnInit {
     iso2: '',
     iso3: '',
     country: '',
-    cities: [
-
-    ]
+    cities: []
   };
 
   cities!: string[];
@@ -42,7 +36,7 @@ export class CrudFormComponent implements OnInit {
   constructor(  private formBuilder: FormBuilder,
                 private countrySvc: CountriesService) {}
 
-  ngOnInit(): void { 
+  ngOnInit ( ): void { 
     this.countrySvc.getCountries()
     .pipe(
       tap( ( countryList: Country[] ) => {this.countries = countryList
@@ -51,44 +45,56 @@ export class CrudFormComponent implements OnInit {
 
       this.crudForm.get("formCountry")?.valueChanges
         .subscribe( city =>{ this.cities = city.cities
-          
-        console.log( city.cities )});
+        console.log( city.cities )
+      }
+      );
     }
 
   crudForm: FormGroup = this.formBuilder.group({
     name: [ '', [ Validators.required, Validators.pattern ( this.namePattern ) ] ],
-    password: [ '', [ Validators.required, Validators.minLength(6), Validators.pattern ( this.passPattern ) ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ],
     passwordConfirm: [ '', [ Validators.required ] ],
     mail: [ '', [ Validators.required, Validators.pattern ( this.mailPattern ) ] ],
-    checkBoxPromotion: [ '' ],
+    checkBoxPromotion: [ '', Validators.required ],
     formCountry: [ '', Validators.required ],
     formCity: [ '',  Validators.required ]
   }, {
     passValidator: [this.passConfirm ( 'password', 'passwordConfirm' ) ]
   });
 
+  launchError ( value: string ) {
+    return this.crudForm.get( value )?.invalid
+    && this.crudForm.get( value )?.touched;
+  }
 
-  launchError(value: string){
-    return this.crudForm.get(value)?.invalid
-    && this.crudForm.get(value)?.touched;
-    }
+  unTouchedInput ( value: string ) {
+    return this.crudForm.get( value )?.untouched;
+  }
+
+  testPass (p1:string, p2:string) {
+    const pa1 = this.crudForm.get( p1 )?.value;
+    this.crudForm.get( p1 )?.touched;
+    const pa2 = this.crudForm.get( p2 )?.value;
+    this.crudForm.get( p1 )?.touched;
+    (pa1 == pa2)
+    ?true 
+    :false;
+  }
   
-  passConfirm( pass: string, passC: string ) {
-    return ( formGroup: AbstractControl ): ValidationErrors | null => {
-      const password = formGroup.get( pass )?.value;
-      const passwordConfirm = formGroup.get( passC )?.value;
+  passConfirm ( p1: string, p2: string) {
+    return (formGroup: AbstractControl ): ValidationErrors | null => {
+      const pass1 = formGroup.get( p1 )?.value;
+      const pass2 = formGroup.get( p2 )?.value;
 
-      if( password !== passwordConfirm ) {
-        formGroup.get( passC )?.setErrors( {
-          notEqual: true 
-        } )
-        return { 
-          notEqual: true 
-        }
+      if( pass1 !== pass2 ) {
+        formGroup.get( p2 )?.setErrors({
+          notEquals: true
+         })
+         return { notEquals : true };
       }
-      formGroup.get( passC )?.setErrors( null );
       return null;
     }
+
   }
 
   onFormSubmit ( ) {
