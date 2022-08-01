817e51de0917e8319d5f1acccb128530ac46bf51
Samuel Fuentes Sánchez <samuel.fuentes@bosonit.com>
2022-08-01 16:27:05 +0200

    angular-13 187

--- angular/05-heroesApp/src/app/material/material.module.ts
+++ angular/05-heroesApp/src/app/material/material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports:[
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
